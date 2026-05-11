e54a3143c6239d1451626fca302b358f5efdb11a
isEduardo13 <eduardoisidroh13@gmail.com>
2026-05-11 16:57:26 -0600

    Agregar archivos de configuración y estructura inicial del proyecto Spring Boot

---
+++ src/main/java/com/carniceria/isidro/isidrocarniceria/IsidrocarniceriaApplication.java
@@ -0,0 +1,13 @@
+package com.carniceria.isidro.isidrocarniceria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsidrocarniceriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsidrocarniceriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carniceria/isidro/isidrocarniceria/IsidrocarniceriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carniceria.isidro.isidrocarniceria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsidrocarniceriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
